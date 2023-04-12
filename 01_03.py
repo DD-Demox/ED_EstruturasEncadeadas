11be7abd60b73ad4b85bebb9c363fda2b51fadca
Armando Asch <armandoasch@gmail.com>
2023-04-12 06:46:21 -0300

    primeiro Commit

---
+++ 01_03.py
@@ -0,0 +1,16 @@
+from listaEncadeada import ListaEncadeada
+
+lista = ListaEncadeada()
+
+lista.inserir_no_inicio(234)
+print(lista)
+lista.inserir_no_inicio(123)
+print(lista)
+lista.append(23)
+print(lista)
+lista.append(50)
+print(lista)
+lista.insere_depois(23,24)
+print(lista)
+lista.remove_com_busca(234)
+print(lista)

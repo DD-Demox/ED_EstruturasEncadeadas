a9559e0a9e26acb6479b9968dcc4fe7150a7b64c
armando <armandoasch@gmail.com>
2023-04-25 15:39:32 -0300

    refatorando codigo

--- 03_04.py
+++ 03_04.py
@@ -2,14 +2,12 @@ from filaEncadeada import Queue
 from listaEncadeada import ListaEncadeada
 
 
-def invert_queue(queue: Queue):
-    list = ListaEncadeada()
-    for i in range(queue.size()):
-        list.inserir_no_inicio(queue.pop())
-    queue.empty_queue()
-    for i in range(list.size()):
-        queue.push(list.get_value_index(i))
-
+def invert_queue(queue_to_invert: Queue):
+    lista = ListaEncadeada()
+    for i in range(queue_to_invert.size()):
+        lista.inserir_no_inicio(queue_to_invert.pop())
+    for i in range(lista.size()):
+        queue_to_invert.push(lista.get_value_index(i))
 
 
 queue = Queue()
